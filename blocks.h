b9d3816d22fc65e249725190677e9d4aa4880915
Charbel Sarkis <charbelsako@gmail.com>
2020-11-13 13:41:07 +0200

    added internet, brightness, nettraf, volume, battery

--- blocks.h
+++ blocks.h
@@ -4,8 +4,11 @@ static const Block blocks[] = {
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{" ", "date '+%a %b %d %I:%M%p'",					5,		0},
-	{"", "~/.local/bin/statusbar/volume", 							1,  		22},
-	{"", "~/.local/bin/statusbar/brightness", 						1, 		23}
+	{"", "~/.local/bin/statusbar/volume", 		5,  		22},
+	{" ", "~/.local/bin/statusbar/brightness", 	1, 		23},
+	{"", "~/.local/bin/statusbar/battery",	1, 23},
+	{"", "~/.local/bin/statusbar/nettraf", 		1, 24 },
+	{"", "~/.local/bin/statusbar/internet", 1, 25}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
